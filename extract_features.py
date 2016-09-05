a5b6a16b15d565be8b64ff66c1d4d0b0ecfd68c4
Colin Ni <colinni1998@gmail.com>
2016-09-04 18:13:20 -0700

    Documentation. Clean up code.

--- extract_features.py
+++ extract_features.py
@@ -3,35 +3,43 @@ import re
 
 def get_engine_eval(game_node):
     '''
-    Returns what the engine evaluated the position in `game_node` to be; None
-    if the move played was taken from the book.
+    Returns what the engine evaluated the position in `game_node` to be;
+    returns None if the move played was taken from the book.
 
-    The returned evaluation is by the engine that played the move that lead
-    to `game_node`; the TCEC updates an engine's eval only after moves
-    it plays -- every other move played.
+    The returned evaluation is bh the engine that played the move that
+    immediately led to `game_node`; the TCEC updates the engines' evaluations
+    only after moves they play.
     '''
 
-    # TCEC's pgn files include engine information -- such as time remaining
-    # and depth searched -- for each move played. `python-chess` considers
-    # this information to be a `comment` of each move.
+    # TCEC's pgn files log engine information such as time remaining
+    # and depth searched for each move played. `python-chess`, when parsing
+    # the pgn files, considers this information to be the `comment` of each
+    # move.
     comment = game_node.comment
 
-    # Search for floats in `comment`. No other piece of information
-    # comes in the form of a float, so this yields the evaluation.
+    # Search for floats in `comment`. No other piece of information logged
+    # by the TCEC comes in the form of a float, so this yields the evaluation.
     engine_eval = re.findall('\d+\.\d+', comment)
     if len(engine_eval) == 0:
         # This move wasn't calculated because it was taken from the book.
         return None
     else:
-        assert len(engine_eval) == 1, 'Using `re.findall` won\'t work.'
+        assert len(engine_eval) == 1
         return engine_eval[0]
 
 
 def _side_to_move(position):
+    '''
+    Returns True if it's White to move.
+    '''
     return [position.turn]
 
 
 def _castling_rights(position):
+    '''
+    Returns True if {White, Black} has castling rights on the
+    {kingside, queenside}.
+    '''
     return [
         position.has_kingside_castling_rights(chess.WHITE),
         position.has_kingside_castling_rights(chess.BLACK),
@@ -41,6 +49,9 @@ def _castling_rights(position):
 
 
 def _material_configuration(position):
+    '''
+
+    '''
     # The first field of the FEN representation of the position. To count
     # the number of each piece in the position, simply count the frequency
     # of its corresponding letter in `board_fen`.
@@ -53,65 +64,68 @@ def _material_configuration(position):
         ]
     ]
 
+def _to_coord(square):
+    '''
+    Returns the row-major coordinate of square. Used by `_piece_lists()`.
+    '''
+    return (square // 8, square % 8)
+
+
+def _get_min_attacker(position, color, square):
+    '''
+    Returns the value of the minimum-valued attacker of `square`; returns 0
+    if `square` is not attacked by any piece.
+    '''
+    return min(
+        (
+            position.piece_type_at(square)
+            for square in position.attackers(color, square)
+        ),
+        default=0
+    )
 
 def _piece_lists(position):
     # There are things in life that you should just take for granted. Do
     # yourself a favor and save me the shame and skip over this method.
-    pieces = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b' ,'r', 'q', 'k']
     piece_freqs = {
-        'P' : 8, 'N' : 2, 'B' : 2, 'R' : 2, 'Q' : 2, 'K' : 1,
-        'p' : 8, 'n' : 2, 'b' : 2 ,'r' : 2, 'q' : 2, 'k' : 1
+        'P' : 8, 'N' : 3, 'B' : 2, 'R' : 2, 'Q' : 2, 'K' : 1,
+        'p' : 8, 'n' : 3, 'b' : 2 ,'r' : 2, 'q' : 2, 'k' : 1
     }
-    piece_coords = { piece : [] for piece in pieces }
-    for i in range(8):
-        for j in range(8):
-            piece = position.piece_at(64 - 8 * (i + 1) + j)
 
-            if piece is not None:
-                piece_color = piece.color
-                min_attacker_and_defender = tuple([
-                    min(
-                        (
-                            position.piece_type_at(square)
-                            for square in position.attackers(
-                                color,
-                                64 - 8 * (i + 1) + j
-                            )
-                        ),
-                        default=0
-                    )
-                    for color in ((chess.BLACK, chess.WHITE) if piece_color == chess.WHITE else (chess.WHITE, chess.BLACK))
-                ])
-                piece_coords[piece.symbol()].append((i, j) + min_attacker_and_defender)
+    piece_coords = { piece : [] for piece in piece_freqs }
+    for square in chess.SQUARES_180:
+        piece = position.piece_at(square)
+        if piece is not None:
+            min_attacker_and_defender = tuple(
+                _get_min_attacker(position, color, square)
+                for color in (not piece.color, piece.color)
+            )
+            piece_coords[piece.symbol()].append(_to_coord(square) + min_attacker_and_defender)
+
     return [
         element
-        for piece in pieces
+        for piece in piece_freqs
         for coord in piece_coords[piece] + [(-1, -1)] * (piece_freqs[piece] - len(piece_coords[piece]))
         for element in coord + (coord != (-1, -1),)
     ]
 
 
 def _sliding_pieces_mobility(position):
-    return []
+    return [[
+        any(
+            position.piece_type_at(move.from_square) == piece
+            for piece in (chess.QUEEN, chess.BISHOP, chess.ROOK)
+        )
+        for move in position.pseudo_legal_moves
+    ].count(True)]
 
 
 def _attack_and_defend_maps(position):
     return [
-        min(
-            (
-                position.piece_type_at(square)
-                for square in position.attackers(
-                    color,
-                    64 - 8 * (i + 1) + j
-                )
-            ),
-            default=0
-        )
+        _get_min_attacker(position, color, square)
         for color in (chess.WHITE, chess.BLACK)
-        for i in range(8)
-        for j in range(8)
+        for square in chess.SQUARES_180
     ]
-
 
 def get_position_features(position):
     features = (
