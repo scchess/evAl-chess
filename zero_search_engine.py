a8c6a2df4fafb08348140541052dcfc7bf89f3c7
Colin Ni <colinni1998@gmail.com>
2016-09-07 23:29:01 -0700

    Backup of current model. Performance ~0.45.

--- zero_search_engine.py
+++ zero_search_engine.py
@@ -11,13 +11,11 @@ def engine_evaluate(position):
     The zero-search engine's evaluation of `position`; a higher number means
     that the engine evaluates that the position favors white.
     '''
-    print(type(position))
     x_unscaled = np.array([extract_features.get_features(position)]).astype(float)
-    print(x_unscaled)
     x_scaled = train.scaler_X.transform(x_unscaled)
-    y_scaled = scaled_evaluation = model.predict(extract_features.split_features(np.array(x_scaled)))
-    y_unscaled = unscaled_evaluation = train.scaler_Y.inverse_transform(np.reshape(y_scaled, (len(y_scaled), 1)))
-    return unscaled_evaluation
+    y_scaled = scaled_evaluation = model.predict(extract_features.split_features(x_scaled))
+    y_unscaled = unscaled_evaluation = train.scaler_Y.inverse_transform(y_scaled)
+    return unscaled_evaluation[0][0]
 
 
 def get_engine_analysis(position):
@@ -57,45 +55,75 @@ def get_engine_move(position, playing_color):
 
 def alpha_beta(position, depth, alpha=-500, beta=+500, color=chess.WHITE):
     if depth == 0:
-        return engine_evaluate(position)
-    elif color == chess.WHITE:
-        v = -500
-        for move in position.legal_moves:
-            position.push(move)
-            v = max(v, alpha_beta(position, depth - 1, alpha, beta, chess.BLACK))
-            alpha = max(alpha, v)
+        _eval = engine_evaluate(position)
+        # print(position, _eval)
+        return _eval, None
+
+    branches = []
+    for move in position.legal_moves:
+        child_position = position.copy()
+        child_position.push(move)
+        branches.append((move, child_position))
+
+    branches_sorted = tuple(
+        sorted(
+            branches,
+            key=(
+                lambda item :
+                    - 2 * item[1].is_check()
+                    - 1 * (position.piece_type_at(item[0].to_square) is not None)
+            )
+        )
+    )
+
+    if color == chess.WHITE:
+        best_eval, best_move = -500, None
+        for move, child_position in branches_sorted:
+            child_eval, _ = alpha_beta(child_position, depth - 1, alpha, beta, chess.BLACK)
+            if child_eval > best_eval:
+                best_eval = child_eval
+                best_move = move
+            alpha = max(alpha, child_eval)
             if beta <= alpha:
                 break
-            position.pop()
-        return v
+        return best_eval, best_move
     elif color == chess.BLACK:
-        v = 500
-        for move in position.legal_moves:
-            position.push(move)
-            v = min(v, alpha_beta(position, depth - 1, alpha, beta, chess.WHITE))
-            beta = min(beta, v)
+        best_eval, best_move = +500, None
+        for move, child_position in branches_sorted:
+            child_eval, _ = alpha_beta(child_position, depth - 1, alpha, beta, chess.WHITE)
+            if child_eval < best_eval:
+                best_eval = child_eval
+                best_move = move
+            beta = min(beta, child_eval)
             if beta <= alpha:
                 break
-            position.pop()
-        return v
+        return best_eval, best_move
 
 def play_engine(verbose=False):
-    position = chess.Board()
 
+    position = chess.Board()
     while not position.is_game_over():
-        engine_move, _eval = get_engine_move(position, chess.WHITE)
-        engine_move, _eval = alpha_beta(position, 2)
+        # engine_move, _eval = get_engine_move(position, chess.WHITE)
+        engine_eval, engine_move = alpha_beta(position, 4)
 
         # if verbose:
         #     for move, _eval in get_engine_analysis(position).items():
         #         print(move, _eval)
         print('Engine played', engine_move)
+        print('Engine evaluation', engine_eval)
         position.push(engine_move)
-        print(position)
+        print(position.__unicode__())
         print()
         player_move = input('Your move: ')
         position.push_san(player_move)
 
-
-
 play_engine(True)
+
+# position = chess.Board()
+
+# moves = ['Nf3', 'd6', 'Ne5', 'dxe5', 'd4', 'e4', 'Qd3', 'exd3', 'exd3']
+# for move in moves:
+#     position.push_san(move)
+#     print(position)
+#     print(engine_evaluate(position))
+#     print()
