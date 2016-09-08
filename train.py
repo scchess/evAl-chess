17a181bfba66aa51088beb07c9ab273e49624c91
Colin Ni <colinni1998@gmail.com>
2016-09-08 10:12:04 -0700

    Update. Still working.

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
+'''
+Work in-progress.
+'''
+
 import numpy as np
 import extract_features
 import itertools
+import math
 
 from sklearn import cross_validation, preprocessing
 from keras.models import Sequential, load_model
@@ -52,35 +57,55 @@ if __name__ == '__main__':
         Y = Y[np.where(np.abs(Y) <= upper)]
         return X, Y
 
+    def __split_and_transform(X, Y):
+        X, Y = scaler_X.transform(X), scaler_Y.transform(np.reshape(Y, (len(Y), 1)))
+        X = extract_features.split_features(X)
+        return X, Y
+
     def get_openings(X, Y):
         opening_positions = np.where(X[:, 5] >= 7)
         return X[opening_positions, :][0], Y[opening_positions]
 
     def __evaluate_model(test_data):
         X_test, Y_test = test_data
-        print(X_test.shape, Y_test.shape)
-        X_test = extract_features.split_features(X_test)
-        X_test, Y_test = scaler_X.transform(X_test), scaler_Y.transform(Y_test)
-        return model.evaluate(X_test, Y_test)
+        _eval = model.evaluate(*__split_and_transform(X_test, Y_test), verbose=0)
+        return _eval, math.sqrt(_eval)
 
 
     def check_test_perf(model, X_test, Y_test):
         print(X_test.shape, Y_test.shape)
-        print('Test score on all positions', __evaluate_model((X_test, Y_test)))
-        print('Test score on material imabalanced positions', __evaluate_model(get_material_imbalanced_positions(X_test, Y_test, 2.5)))
-        print('Test score on ground truth range 3-15', __evaluate_model(get_range_positions(X_test, Y_test, 3, 15)))
-        print('Test score on ground truth range 1-3', __evaluate_model(get_range_positions(X_test, Y_test, 1, 3)))
-        print('Test score on ground truth range 0.1-1', __evaluate_model(get_range_positions(X_test, Y_test, 0.1, 1)))
-        print('Test score on openings', __evaluate_model(get_openings(X_test, Y_test)))
+        print('Test score on all positions:', __evaluate_model((X_test, Y_test)))
+        print('Test score on material imabalanced positions:', __evaluate_model(get_material_imbalanced_positions(X_test, Y_test, 2.5)))
+        print('Test score on ground truth range 3-15:', __evaluate_model(get_range_positions(X_test, Y_test, 3, 15)))
+        print('Test score on ground truth range 1-3:', __evaluate_model(get_range_positions(X_test, Y_test, 1, 3)))
+        print('Test score on ground truth range 0.1-1:', __evaluate_model(get_range_positions(X_test, Y_test, 0.1, 1)))
+        print('Test score on ground truth range 0.1-15:', __evaluate_model(get_range_positions(X_test, Y_test, 0.1, 15)))
+        print('Test score on openings:', __evaluate_model(get_openings(X_test, Y_test)))
 
-
-    # X_train, Y_train = get_material_imbalanced_positions(X_train, Y_train, 2.5)
-    X_train, Y_train = get_range_positions(X_train, Y_train, 0.05, 15)
-
-    X_train = scaler_X.transform(X_train)
-    Y_train = scaler_Y.transform(np.reshape(Y_train, (len(Y_train), 1)))
-    X_train = extract_features.split_features(X_train)
-
+    def select_training_data(X_train, Y_train, num):
+        if num == 1: # All data.
+            X_train, Y_train = __split_and_transform(X_train, Y_train)
+            return X_train, Y_train
+        elif num == 2:
+            X_train, Y_train = __split_and_transform(*get_material_imbalanced_positions(X_train, Y_train, 2.5))
+            return X_train, Y_train
+        elif num == 3:
+            X_train, Y_train = __split_and_transform(*get_range_positions(X_train, Y_train, 3, 15))
+            return X_train, Y_train
+        elif num == 4:
+            X_train, Y_train = __split_and_transform(*get_range_positions(X_train, Y_train, 1, 3))
+            return X_train, Y_train
+        elif num == 5:
+            X_train, Y_train = __split_and_transform(*get_range_positions(X_train, Y_train, 0.1, 1))
+            return X_train, Y_train
+        elif num == 6:
+            X_train, Y_train = __split_and_transform(*get_range_positions(X_train, Y_train, 0.1, 15))
+            return X_train, Y_train
+        elif num == 7:
+            X_train, Y_train = __split_and_transform(*get_openings(X_train, Y_train))
+            return X_train, Y_train
+        else:
+            raise ValueError
 
 
     # model = Sequential(
@@ -98,7 +123,7 @@ if __name__ == '__main__':
     #                 Sequential(
     #                     [
     #                         Dense(
-    #                             output_dim=128,
+    #                             output_dim=63,
     #                             input_dim=244
     #                         )
     #                     ]
@@ -106,7 +131,7 @@ if __name__ == '__main__':
     #                 Sequential(
     #                     [
     #                         Dense(
-    #                             output_dim=64,
+    #                             output_dim=32,
     #                             input_dim=128
     #                         )
     #                     ]
@@ -116,18 +141,17 @@ if __name__ == '__main__':
     #         ),
     #         Dropout(0.05),
     #         Activation('relu'),
-    #         Dense(output_dim=64),
-    #         Dropout(0.05),
+    #         Dense(output_dim=32),
     #         Activation('relu'),
     #         Dense(output_dim=1),
     #         Activation('relu')
     #     ]
     # )
     #
-    # model.summary()
     # model.compile(loss='mean_squared_error', optimizer='sgd')
 
-    model = load_model('/Users/colinni/evAl-chess/saved_keras_model_merged_first.h5')
+    model = load_model('/Users/colinni/evAl-chess/saved_keras_model_merged_third.h5')
+    model.summary()
 
     while True:
         inp = input('Continue training for how many epochs (\'s\' to stop and save, t to check test performance)? ')
@@ -136,14 +160,14 @@ if __name__ == '__main__':
         if inp == 't':
             check_test_perf(model, X_test, Y_test)
             continue
+        int(inp)
         bs = input('Batch size? ')
+        num = int(input('Using what data? '))
         history = model.fit(
-            X_train,
-            Y_train,
+            *select_training_data(X_train, Y_train, num),
             batch_size=int(bs),
             nb_epoch=int(inp),
             verbose=1
         )
 
-    model.save('/Users/colinni/evAl-chess/saved_keras_model_merged_first.h5')
-    check_test_perf(model, X_test, Y_test)
+    model.save('/Users/colinni/evAl-chess/saved_keras_model_merged_fourth.h5')
