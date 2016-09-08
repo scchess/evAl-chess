79f12c38ceaced6fc1e59e83b4c8e38bd8599c46
Colin Ni <colinni1998@gmail.com>
2016-09-07 23:29:22 -0700

    Add methods to ease training.

--- train.py
+++ train.py
@@ -12,12 +12,7 @@ np.random.seed(1337)
 X = np.load('/Users/colinni/evAl-chess/X.npy')
 Y = np.load('/Users/colinni/evAl-chess/Y.npy')
 
-# Discard samples where the evaluation is out of [-10, +10].
-X = X[np.where(np.abs(Y) < 35.0)]
-Y = Y[np.where(np.abs(Y) < 35.0)]
-# X = X[np.where(np.abs(Y) > 0.1)]
-# Y = Y[np.where(np.abs(Y) > 0.1)]
-# opening_positions = np.where(X[:, 5] >= 7) # X = X[opening_positions, :][0]
+print('X, Y shape.')
 print(X.shape)
 print(Y.shape)
 
@@ -35,32 +30,58 @@ X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(
 
 scaler_X, scaler_Y = preprocessing.StandardScaler(), preprocessing.StandardScaler()
 # Scale based only on training data.
-X_train, Y_train = scaler_X.fit_transform(X_train), scaler_Y.fit_transform(np.reshape(Y_train, (len(Y_train), 1)))
+scaler_X.fit(X_train)
+scaler_Y.fit(np.reshape(Y_train, (len(Y_train), 1)))
 
-X_test, Y_test = scaler_X.transform(X_test), scaler_Y.transform(np.reshape(Y_test, (len(Y_test), 1)))
-
-def check_test_perf(model, X_test, Y_test):
-    score = model.evaluate(X_test, Y_test, verbose=1)
-    print('Test score', score)
-
-    predictions = scaler_Y.inverse_transform(model.predict(X_test)) * abs(scaler_Y.inverse_transform(model.predict(X_test)))
-    Y_test = scaler_Y.inverse_transform(Y_test) * np.abs(scaler_Y.inverse_transform(Y_test))
-    print(len(predictions), Y_test.shape)
-    for i in range(64):
-        print(round(float(predictions[i]), 3), round(float(Y_test[i]), 3), round(float(Y_test[i]) - float(predictions[i]), 3), sep='\t')
 
 if __name__ == '__main__':
 
-    print(X_train.shape)
-    print(Y_train.shape)
-    print(X_test.shape)
-    print(Y_test.shape)
 
-    print(X_train)
-    print(Y_train)
+    def material_count(config):
+        return 1 * config[0] + 3 * config[1] + 3 * config[2] + 5 * config[3] + 9 * config[4]
+
+    def get_material_imbalanced_positions(X, Y, material_diff):
+        material_diffs = np.array([abs(material_count(sample[5:11]) - material_count(sample[11:17])) for sample in X])
+        material_diff_positions = np.where(material_diffs >= material_diff)
+        return X[material_diff_positions, :][0], Y[material_diff_positions]
+
+    def get_range_positions(X, Y, lower, upper):
+        X = X[np.where(lower <= np.abs(Y))]
+        Y = Y[np.where(lower <= np.abs(Y))]
+        X = X[np.where(np.abs(Y) <= upper)]
+        Y = Y[np.where(np.abs(Y) <= upper)]
+        return X, Y
+
+    def get_openings(X, Y):
+        opening_positions = np.where(X[:, 5] >= 7)
+        return X[opening_positions, :][0], Y[opening_positions]
+
+    def __evaluate_model(test_data):
+        X_test, Y_test = test_data
+        print(X_test.shape, Y_test.shape)
+        X_test = extract_features.split_features(X_test)
+        X_test, Y_test = scaler_X.transform(X_test), scaler_Y.transform(Y_test)
+        return model.evaluate(X_test, Y_test)
 
 
-    X_train, X_test = extract_features.split_features(X_train), extract_features.split_features(X_test)
+    def check_test_perf(model, X_test, Y_test):
+        print(X_test.shape, Y_test.shape)
+        print('Test score on all positions', __evaluate_model((X_test, Y_test)))
+        print('Test score on material imabalanced positions', __evaluate_model(get_material_imbalanced_positions(X_test, Y_test, 2.5)))
+        print('Test score on ground truth range 3-15', __evaluate_model(get_range_positions(X_test, Y_test, 3, 15)))
+        print('Test score on ground truth range 1-3', __evaluate_model(get_range_positions(X_test, Y_test, 1, 3)))
+        print('Test score on ground truth range 0.1-1', __evaluate_model(get_range_positions(X_test, Y_test, 0.1, 1)))
+        print('Test score on openings', __evaluate_model(get_openings(X_test, Y_test)))
+
+
+    # X_train, Y_train = get_material_imbalanced_positions(X_train, Y_train, 2.5)
+    X_train, Y_train = get_range_positions(X_train, Y_train, 0.05, 15)
+
+    X_train = scaler_X.transform(X_train)
+    Y_train = scaler_Y.transform(np.reshape(Y_train, (len(Y_train), 1)))
+    X_train = extract_features.split_features(X_train)
+
+
 
     # model = Sequential(
     #     [
